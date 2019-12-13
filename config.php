b533404a9d03182de2e458f874681ff150e0b056
alf <alf@gmail.com>
2019-12-13 16:21:25 +0700

    Fixed Error without $

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ define('DB_NAME', 'mpp');
 
 
 $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+//mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 if($mysqli === false){
     die("ERROR: Could not connect". $mysqli->connect_error);
