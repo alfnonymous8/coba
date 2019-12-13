2262339f4859c965287aacbd313914b138a5e081
alf <alf@gmail.com>
2019-12-13 16:23:35 +0700

    Error without $

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'mpp');
 
 
-$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-//mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
 
 if($mysqli === false){
     die("ERROR: Could not connect". $mysqli->connect_error);
